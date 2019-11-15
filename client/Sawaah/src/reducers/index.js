1f59c5f32624399c2521f388d3b15c9e43293664
95Rawan <youlostme17@hotmail.com>
2019-11-15 16:42:39 +0200

    Changed file structure and removed unnecessary code Put react native app inside a client folder so we would have a client and server folders outside and it would be more clear. Added redux reducers and actions folders. Added a provider to the app file to have a redux store.

---
+++ client/Sawaah/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case '':
+      return;
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
