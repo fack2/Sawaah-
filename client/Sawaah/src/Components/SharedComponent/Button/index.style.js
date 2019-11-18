2d230ed2ca38b824489da3cb56554006eb8fd0a4
95Rawan <youlostme17@hotmail.com>
2019-11-18 09:09:59 +0200

    Used styled components to style the Button component

---
+++ client/Sawaah/src/Components/SharedComponent/Button/index.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ButtonWrapper = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+  width: 165px;
+  height: 50px;
+  background: #2e5d6a;
+  margin-top: 60px;
+  border-radius: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'Signika';
+  font-size: 17px;
+  color: #ffffff;
+`;
