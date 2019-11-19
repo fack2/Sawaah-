4120017e923342672154bed98ead1332ebc878d9
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-11-19 10:00:35 +0200

    resolve conflict

---
+++ client/Sawaah/src/Components/Layout/Destination/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+class Destination extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+export default Destination;
