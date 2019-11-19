6b3487463dd023cc654e5a49afb01b74c156ab3a
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-11-20 01:54:44 +0200

    Imported Login  on ScreenNavigator Imported the Login on the stack Navigation to display it in the app

--- client/Sawaah/ScreenNavigator.js
+++ client/Sawaah/ScreenNavigator.js
@@ -3,12 +3,14 @@ import {createStackNavigator} from 'react-navigation-stack';
 import LandingPage from './src/Components/Layout/LandingPage';
 import Destination from './src/Components/Layout/Destination';
 import SearchResult from './src/Components/SharedComponent/SearchResult';
+import Login from './src/Components/Layout/Login';
 
 const NavigationStack = createStackNavigator(
   {
     LandingPage: {screen: LandingPage},
     Destination: {screen: Destination},
     SearchResult: {screen: SearchResult},
+    Login: {screen: Login},
   },
   {initialRouteName: 'LandingPage'},
   {
