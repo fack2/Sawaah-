1ee002b088167af52d89224a5c55c168b17964de
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-11-19 11:34:22 +0200

    Imported  SearchResult screen on NavigationStack

--- client/Sawaah/ScreenNavigator.js
+++ client/Sawaah/ScreenNavigator.js
@@ -2,11 +2,13 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import LandingPage from './src/Components/Layout/LandingPage';
 import Destination from './src/Components/Layout/Destination';
+import SearchResult from './src/Components/Layout/SearchResult';
 
 const NavigationStack = createStackNavigator(
   {
     LandingPage: {screen: LandingPage},
     Destination: {screen: Destination},
+    SearchResult: {screen: SearchResult},
   },
   {initialRouteName: 'LandingPage'},
 );
