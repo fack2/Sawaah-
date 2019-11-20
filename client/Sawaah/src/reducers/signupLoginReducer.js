4af366789ec105588d16265d2ed3b2298baa56a2
95Rawan <youlostme17@hotmail.com>
2019-11-20 10:56:09 +0200

    Returned state for each case

--- client/Sawaah/src/reducers/signupLoginReducer.js
+++ client/Sawaah/src/reducers/signupLoginReducer.js
@@ -9,18 +9,22 @@ const signupLoginReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CHANGE_USER_NAME_SIGNUP':
       return {
+        ...state,
         userName: action.payload,
       };
     case 'CHANGE_USER_EMAIL_SIGNUP':
       return {
+        ...state,
         userEmail: action.payload,
       };
     case 'CHANGE_USER_PASSWORD_SIGNUP':
       return {
+        ...state,
         userPassword: action.payload,
       };
     case 'CHANGE_USER_CONFIRM_PASSWORD_SIGNUP':
       return {
+        ...state,
         userConfirmPassword: action.payload,
       };
 
