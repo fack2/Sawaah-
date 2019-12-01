a6b5b886304d6105409212879454ec58a56cc6aa
95Rawan <youlostme17@hotmail.com>
2019-12-01 14:23:48 +0200

    Added new actions Added getDestinationCountries to handle requesting destination table data from the back-end. Added enableDateDropdownList to handle the activation of the date dropdown list. Added enableClassDropdownList to handle the activation of the class dropdown list. Changed the naming of the actions and their creators.

--- client/Sawaah/src/actions/defaultAppActions.js
+++ client/Sawaah/src/actions/defaultAppActions.js
@@ -1,33 +1,107 @@
-export const countrySelector = payload => {
+import axios from 'axios';
+
+export const getDestinationCountries = () => async dispatch => {
+  try {
+    const response = await axios.get(
+      'http://192.168.1.168:4000/api/destination',
+    );
+
+    const payload = response.data;
+
+    dispatch({
+      type: 'DESTINATION_COUNTRIES_GET',
+      payload,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const selectCountry = payload => {
   return {
-    type: 'SELECT_COUNTRY',
+    type: 'COUNTRY_SELECT',
     payload,
   };
 };
 
-export const dateSelector = payload => {
+export const selectDate = payload => {
   return {
-    type: 'SELECT_DATE',
-    payload,
-  };
-};
-export const classTypeSelector = payload => {
-  return {
-    type: 'SELECT_CLASSTYPE',
+    type: 'DATE_SELECT',
     payload,
   };
 };
 
-export const adultsNumberChange = payload => {
+export const selectClassType = payload => {
   return {
-    type: 'CHANGE_ADULTS_NUMBER',
+    type: 'CLASSTYPE_SELECT',
     payload,
   };
 };
 
-export const childrenNumberChange = payload => {
+export const changeAdultsNumber = payload => {
   return {
-    type: 'CHANGE_CHILDREN_NUMBER',
+    type: 'ADULTS_NUMBER_CHANGE',
     payload,
   };
+};
+
+export const changeChildrenNumber = payload => {
+  return {
+    type: 'CHILDREN_NUMBER_CHANGE',
+    payload,
+  };
+};
+
+export const enableDateDropdownList = () => (dispatch, getState) => {
+  try {
+    let enabled = '';
+    const {country, date} = getState().defaultAppReducer;
+
+    if (country !== 'Country') {
+      enabled = true;
+
+      dispatch({
+        type: 'DATE_DROPDOWN_LIST_ENABLE',
+        enabled,
+        date,
+      });
+    } else {
+      enabled = false;
+
+      dispatch({
+        type: 'DATE_DROPDOWN_LIST_ENABLE',
+        enabled,
+        date: 'Date',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const enableClassDropdownList = () => (dispatch, getState) => {
+  try {
+    let enabled = '';
+    const {date, classType} = getState().defaultAppReducer;
+
+    if (date !== 'Date') {
+      enabled = true;
+
+      dispatch({
+        type: 'CLASS_DROPDOWN_LIST_ENABLE',
+        enabled,
+        classType,
+      });
+    } else {
+      enabled = false;
+
+      dispatch({
+        type: 'CLASS_DROPDOWN_LIST_ENABLE',
+        enabled,
+        classType: 'Class',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
