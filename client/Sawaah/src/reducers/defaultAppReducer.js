2d494ef51d77659555ebf0a84b87198701c01ad1
95Rawan <youlostme17@hotmail.com>
2019-12-01 14:08:28 +0200

    Added new cases to defaultAppReducer to handle the activation of dropdown lists and changed naming of existing cases

--- client/Sawaah/src/reducers/defaultAppReducer.js
+++ client/Sawaah/src/reducers/defaultAppReducer.js
@@ -1,37 +1,65 @@
 const initialState = {
-  country: '',
-  date: '',
-  classType: '',
+  destinationCountries: [],
+  country: 'Country',
+  date: 'Date',
+  classType: 'Class',
   adultsNumber: '',
   childrenNumber: '',
+  dateDropdownListEnabled: false,
+  classDropdownListEnabled: false,
+  ticketResults: [],
 };
 
 const defaultAppReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SELECT_COUNTRY':
+    case 'DESTINATION_COUNTRIES_GET':
+      return {
+        ...state,
+        destinationCountries: action.payload,
+      };
+
+    case 'COUNTRY_SELECT':
       return {
         ...state,
         country: action.payload,
       };
-    case 'SELECT_DATE':
+
+    case 'DATE_SELECT':
       return {
         ...state,
         date: action.payload,
       };
-    case 'SELECT_CLASSTYPE':
+
+    case 'CLASSTYPE_SELECT':
       return {
         ...state,
         classType: action.payload,
       };
-    case 'CHANGE_ADULTS_NUMBER':
+
+    case 'ADULTS_NUMBER_CHANGE':
       return {
         ...state,
         adultsNumber: action.payload,
       };
-    case 'CHANGE_CHILDREN_NUMBER':
+
+    case 'CHILDREN_NUMBER_CHANGE':
       return {
         ...state,
         childrenNumber: action.payload,
+      };
+
+    case 'DATE_DROPDOWN_LIST_ENABLE':
+      return {
+        ...state,
+        dateDropdownListEnabled: action.enabled,
+        date: action.date,
+      };
+
+    case 'CLASS_DROPDOWN_LIST_ENABLE':
+      return {
+        ...state,
+        classDropdownListEnabled: action.enabled,
+        classType: action.classType,
       };
 
     default:
