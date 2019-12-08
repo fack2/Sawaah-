c91629a8fe88a014c03b5e5371bf00a95a8a32d8
95Rawan <youlostme17@hotmail.com>
2019-12-08 20:33:13 +0200

    Added a container to put all the cards information in and removed extra container

--- client/Sawaah/src/Components/Layout/Profile/index.js
+++ client/Sawaah/src/Components/Layout/Profile/index.js
@@ -8,7 +8,7 @@ import {
   Ticket,
   CompanyLogo,
   CardTicket,
-  SharedContainer,
+  TicketInfoContainer,
   BaseContainer,
   DisplayText,
   ResultText,
@@ -16,6 +16,7 @@ import {
   AirplanImg,
   BarCodeImg,
 } from './index.style';
+import {ScrollView} from 'react-native-gesture-handler';
 
 class Profile extends React.Component {
   static navigationOptions = {
@@ -26,76 +27,76 @@ class Profile extends React.Component {
 
   render() {
     return (
-      <PageContainer>
-        <ProfileImg
-          source={require('./../../../assets/images/profileImg.png')}
-        />
-        <UserPersonalInformation>Brianca Davis</UserPersonalInformation>
-        <UserPersonalInformation>
-          briancadavis@hotmail.com
-        </UserPersonalInformation>
-        <Header>Your Ticket </Header>
-        <CardTicket>
-          <CompanyLogo
-            source={require('./../../../assets/images/PA-logo.gif')}
+      <ScrollView>
+        <PageContainer>
+          <ProfileImg
+            source={require('./../../../assets/images/profileImg.png')}
           />
-          <Ticket source={require('./../../../assets/images/profile.png')} />
-          <SharedContainer>
-            <BaseContainer>
-              <SubContainer>
-                <DisplayText>From</DisplayText>
-                <ResultText>PNA</ResultText>
-              </SubContainer>
-              <AirplanImg
-                source={require('./../../../assets/images/plane.png')}
-              />
-              <SubContainer>
-                <DisplayText>To</DisplayText>
-                <ResultText>BNA</ResultText>
-              </SubContainer>
-            </BaseContainer>
-          </SharedContainer>
-          <SharedContainer>
-            <BaseContainer>
-              <SubContainer>
-                <DisplayText>Departure</DisplayText>
-                <ResultText>4:30 pm</ResultText>
-              </SubContainer>
-              <SubContainer>
-                <DisplayText>Arrival</DisplayText>
-                <ResultText>11:00 am</ResultText>
-              </SubContainer>
-            </BaseContainer>
-          </SharedContainer>
-          <SharedContainer>
-            <BaseContainer>
-              <SubContainer>
-                <DisplayText>Class</DisplayText>
-                <ResultText>Business</ResultText>
-              </SubContainer>
-              <SubContainer>
-                <DisplayText>Seat</DisplayText>
-                <ResultText>B4</ResultText>
-              </SubContainer>
-            </BaseContainer>
-          </SharedContainer>
-          <SharedContainer>
-            <BaseContainer>
-              <SubContainer>
-                <DisplayText>Flight No</DisplayText>
-                <ResultText>S65</ResultText>
-              </SubContainer>
-              <SubContainer>
-                <DisplayText>Gate</DisplayText>
-                <ResultText>10L</ResultText>
-              </SubContainer>
-            </BaseContainer>
-          </SharedContainer>
-          <BarCodeImg
-            source={require('./../../../assets/images/barcode.png')}
-          />
-        </CardTicket>
-      </PageContainer>
+          <UserPersonalInformation>Brianca Davis</UserPersonalInformation>
+          <UserPersonalInformation>
+            briancadavis@hotmail.com
+          </UserPersonalInformation>
+          <Header>Your Ticket </Header>
+          <CardTicket>
+            <CompanyLogo
+              source={require('./../../../assets/images/PA-logo.gif')}
+            />
+            <Ticket source={require('./../../../assets/images/profile.png')} />
+
+            <TicketInfoContainer>
+              <BaseContainer>
+                <SubContainer>
+                  <DisplayText>From</DisplayText>
+                  <ResultText>PNA</ResultText>
+                </SubContainer>
+                <AirplanImg
+                  source={require('./../../../assets/images/plane.png')}
+                />
+                <SubContainer>
+                  <DisplayText>To</DisplayText>
+                  <ResultText>BNA</ResultText>
+                </SubContainer>
+              </BaseContainer>
+
+              <BaseContainer>
+                <SubContainer>
+                  <DisplayText>Departure</DisplayText>
+                  <ResultText>4:30 pm</ResultText>
+                </SubContainer>
+                <SubContainer>
+                  <DisplayText>Arrival</DisplayText>
+                  <ResultText>11:00 am</ResultText>
+                </SubContainer>
+              </BaseContainer>
+
+              <BaseContainer>
+                <SubContainer>
+                  <DisplayText>Class</DisplayText>
+                  <ResultText>Business</ResultText>
+                </SubContainer>
+                <SubContainer>
+                  <DisplayText>Seat</DisplayText>
+                  <ResultText>B4</ResultText>
+                </SubContainer>
+              </BaseContainer>
+
+              <BaseContainer>
+                <SubContainer>
+                  <DisplayText>Flight No</DisplayText>
+                  <ResultText>S65</ResultText>
+                </SubContainer>
+                <SubContainer>
+                  <DisplayText>Gate</DisplayText>
+                  <ResultText>10L</ResultText>
+                </SubContainer>
+              </BaseContainer>
+            </TicketInfoContainer>
+            <BarCodeImg
+              source={require('./../../../assets/images/barcode.png')}
+            />
+          </CardTicket>
+        </PageContainer>
+      </ScrollView>
     );
   }
 }
